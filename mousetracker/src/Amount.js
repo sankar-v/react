79d53f2e4e0e9706920cad556168021f642f5a5e
Sankar V <s_venkat_99@yahoo.co.uk>
2018-12-25 06:43:37 +0530

    Fixed errors in arg in Pound, Stateless Amount

--- mousetracker/src/Amount.js
+++ mousetracker/src/Amount.js
@@ -2,8 +2,23 @@ import React from 'react';
 //https://www.robinwieruch.de/react-render-props-pattern/
 
 const Euro = ({amount}) => <p>Euro: {amount * 0.86}</p>;
-const Pound = (amount) => <p>Pound: {amount * 0.76}</p>;
+const Pound = ({amount}) => <p>Pound: {amount * 0.76}</p>;
 
+const Amount = ({amount, onIncrement, onDecrement}) =>
+(
+    <div>
+    <span>US Dollar: {amount} </span>
+
+    <button type="button" onClick={onIncrement}>
+      +
+    </button>
+    <button type="button" onClick={onDecrement}>
+      -
+    </button>
+  </div>
+);
+
+/*
 class Amount extends React.Component{
     constructor(props){
         super(props);
@@ -27,5 +42,5 @@ class Amount extends React.Component{
         );
     }
 }
-
+*/
 export {Euro, Pound, Amount};
