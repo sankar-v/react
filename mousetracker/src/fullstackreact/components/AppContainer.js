2bbe1c4ecd914470dd2c809fb6ced09f386c33ab
Sankar V <s_venkat_99@yahoo.co.uk>
2018-12-29 04:59:33 +0530

    Basic content class with activity prop

--- mousetracker/src/fullstackreact/components/AppContainer.js
+++ mousetracker/src/fullstackreact/components/AppContainer.js
@@ -2,12 +2,23 @@ import React from 'react';
 import Content from './Content';
 import Header from './Header';
 
+const activity = {
+        timestamp: new Date().getTime(),
+        text: "Ate lunch",
+        user: {
+          id: 1,
+          name: 'Nate',
+          avatar: "http://www.croop.cl/UI/twitter/images/doug.jpg"
+        },
+        comments: [
+          { from: 'Ari', text: 'Me too!' }
+        ]
+}
+
 const AppContainer =() => (
     <div>
         <Header title="Timeline" />
-        <Header title="Profile" />
-        <Header title="Settings" />
-        <Header title="Chat" />
+        <Content activity={activity} />
     </div>
 )
 
--- mousetracker/src/fullstackreact/components/Content.js
+++ mousetracker/src/fullstackreact/components/Content.js
@@ -1,27 +1,28 @@
 import React from 'react';
 
 class Content extends React.Component {
+
     render() {
-      return (
+        const {activity} = this.props;//ES6 destructuring
+        return (
         <div className="content">
           <div className="line"></div>
   
-          {/* Timeline item */}
           <div className="item">
-            <div className="avatar">
-              <img
-              alt='Doug'
-              src="http://www.croop.cl/UI/twitter/images/doug.jpg" />
-              Doug
-            </div>
-  
-            <span className="time">
-              An hour ago
-            </span>
-            <p>Ate lunch</p>
-            <div className="commentCount">
-              2
-            </div>
+          <div className="avatar">
+            <img
+              alt={activity.text}
+              src={activity.user.avatar} />
+            {activity.user.name}
+          </div>
+
+          <span className="time">
+            {activity.timestamp}
+          </span>
+          <p>{activity.text}</p>
+          <div className="commentCount">
+            {activity.comments.length}
+          </div>
           </div>
   
           {/* ... */}
