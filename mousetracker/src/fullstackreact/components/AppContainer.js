ef555f4c7ee4571d108c917c295c338cafa3328d
Sankar V <s_venkat_99@yahoo.co.uk>
2018-12-29 19:21:33 +0530

    fixed the issue with array map..passing single actviity to  Content component

--- mousetracker/src/fullstackreact/components/AppContainer.js
+++ mousetracker/src/fullstackreact/components/AppContainer.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import Content from './Content';
 import Header from './Header';
+import Content from './Content';
 
 const activity = {
         timestamp: new Date().getTime(),
@@ -15,11 +15,72 @@ const activity = {
         ]
 }
 
+const activities = [
+    {
+      timestamp: new Date().getTime(),
+      text: "Ate lunch",
+      user: {
+        id: 1, name: 'Nate',
+        avatar: "http://www.croop.cl/UI/twitter/images/doug.jpg"
+      },
+      comments: [{ from: 'Ari', text: 'Me too!' }]
+    },
+    {
+      timestamp: new Date().getTime(),
+      text: "Woke up early for a beautiful run",
+      user: {
+        id: 2, name: 'Ari',
+        avatar: "http://www.croop.cl/UI/twitter/images/doug.jpg"
+      },
+      comments: [{ from: 'Nate', text: 'I am so jealous' }]
+    },
+    {
+        timestamp: new Date().getTime(),
+        text: "Ate lunch",
+        user: {
+          id: 1, name: 'Nate',
+          avatar: "http://www.croop.cl/UI/twitter/images/doug.jpg"
+        },
+        comments: [{ from: 'Ari', text: 'Me too!' }]
+      },
+      {
+        timestamp: new Date().getTime(),
+        text: "Woke up early for a beautiful run",
+        user: {
+          id: 2, name: 'Ari',
+          avatar: "http://www.croop.cl/UI/twitter/images/doug.jpg"
+        },
+        comments: [{ from: 'Nate', text: 'I am so jealous' }]
+      },
+];
+
 const AppContainer =() => (
     <div>
         <Header title="Timeline" />
-        <Content activity={activity} />
+        <Content activity = {activity}/>
     </div>
 )
 
-export default AppContainer;
+class AppContainer1 extends React.Component{
+    constructor(props){
+        super(props);
+    }
+
+    render(){
+        return(
+            <div>
+            {
+            activities.map((a) => {
+               return(
+               <div> 
+                   <Content activity = {a}/>
+                </div>
+               )
+            })
+            }
+            </div>
+        )
+    };
+}
+
+export default AppContainer1;
