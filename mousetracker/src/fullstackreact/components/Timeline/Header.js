14d414b205fc55d1de17f10a6365c1b2548ed3d5
Sankar V <s_venkat_99@yahoo.co.uk>
2019-01-01 20:56:04 +0530

    Inline styles for Header

--- mousetracker/src/fullstackreact/components/Timeline/Header.js
+++ mousetracker/src/fullstackreact/components/Timeline/Header.js
@@ -1,48 +1,67 @@
 import React from 'react';
 
-class Header extends React.Component{
+class Header extends React.Component {
+  constructor(props) {
+    super(props);
 
-    constructor(props){
-        super(props);
-        this.state = { inputText: 'Type your text here...',};
+    this.state = {
+      searchVisible: false
+    }
+  }
+
+  // toggle visibility when run on the state
+  showSearch() {
+    this.setState({
+      searchVisible: !this.state.searchVisible
+    })
+  }
+
+  render() {
+    // Classes to add to the <input /> element
+    let searchInputClasses = ["searchInput"];
+
+    // Update the class array if the state is visible
+    if (this.state.searchVisible) {
+      searchInputClasses.push("active");
     }
 
-    getInitialState(){
-        return {value: 'Hello!'};
+    const wrapperStyle = {
+      backgroundColor: 'rgba(251, 202, 43, 1)'
     }
-    /*
-    handleChange = (event) =>{
-        const v = event.target.value;
-        alert(v);
-        this.setState(state => ({inputText: {v}}));
-    }
-    */
-    render(){
-        //class to add to the <input/> element
-        let searchInputClasses = ["searchInput"];
-        
-        //Update the class array if the state is visible
-        if (this.state.searchVisible) {
-            searchInputClasses.push("active");
-        }
-          
-        return(
-            <div className="header">
-                <div className="fa fa-more"></div>
 
-                <span className="title">
-                {this.props.title}
-                </span>
-                                
-                <input 
-                    type="text"
-                    className={searchInputClasses.join(' ')}
-                    placeholder="Search ..." />
-      
-                <div className="fa fa-search searchIcon"></div>
-            </div>
-        )
+    const titleStyle = {
+      color: '#111111'
     }
+
+    const menuColor = {
+      backgroundColor: '#111111'
+    }
+
+    return (
+      <div style={wrapperStyle} className="header">
+        <div className="menuIcon">
+          <div className="dashTop" style={menuColor}></div>
+          <div className="dashBottom" style={menuColor}></div>
+          <div className="circle" style={menuColor}></div>
+        </div>
+
+        <span style={titleStyle} className="title">
+          {this.props.title}
+        </span>
+
+        <input
+          type="text"
+          className={searchInputClasses.join(' ')}
+          placeholder="Search ..." />
+
+        {/* Adding an onClick handler to call the showSearch button */}
+        <div
+          style={titleStyle}
+          onClick={this.showSearch.bind(this)}
+          className="fa fa-search searchIcon"></div>
+      </div>
+    )
+  }
 }
 
-export default Header;
+export default Header
