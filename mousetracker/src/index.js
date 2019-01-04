8c57533ea0d0478ab0ed0e4d66130a37cbbe09b4
Sankar V <s_venkat_99@yahoo.co.uk>
2019-01-04 20:17:58 +0530

    Spread operator and object destructuring

--- mousetracker/src/index.js
+++ mousetracker/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-
+import Formatter from './fullstackreact/components/Timer/Formatter';
 
 import {Hour, Minute, Second} from './fullstackreact/components/Timer/Hour';
 import Clock from './fullstackreact/components/Timer/FormattedClock';
@@ -20,13 +20,41 @@ export const load = () => {
   );
 };
 */
+/*
   ReactDOM.render(
     <div className="clock">
       <Clock format="h:m:s p" />
     </div>,
     document.getElementById('demo2')
   );
+*/
+const student = {
+    name: 'hello',
+    age: '30',
+    sex: 'M',
+    year: '2000'
+};
 
+const Display = ({age}) => (
+    <span>{age}</span>
+)
+
+const CalculateRaise = ({sex, age, ratio=2}) => {
+    if (sex == 'M' && age < 100 && ratio === 2){
+        return <span>Raise permitted</span>
+    }else{
+        return <span>Raise NOT permitted</span>
+    }
+}
+
+ReactDOM.render(<Display {...student} />, document.getElementById('demo1'));
+ReactDOM.render(<CalculateRaise {...student}/>, document.getElementById('demo2'));
+/*ReactDOM.render(<Formatter format="h:m:s a" 
+                            hours={12}  
+                            minutes={10} 
+                            seconds={30}/>, 
+                            document.getElementById('demo2'));
+                            */
 /*
 try {
   load();
