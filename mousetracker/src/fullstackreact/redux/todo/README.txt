ba47a354da0e410f97eff1f802d294b67ec2fe78
Sankar V <s_venkat_99@yahoo.co.uk>
2019-01-14 16:51:29 +0530

    Todo - redux example - initial

---
+++ mousetracker/src/fullstackreact/redux/todo/README.txt
@@ -0,0 +1,2 @@
+Basic example taken from redux.js.org
+https://redux.js.org/basics/example
---
+++ mousetracker/src/fullstackreact/redux/todo/components/Todo.js
@@ -0,0 +1,23 @@
+//https://redux.js.org/basics/example
+
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const Todo = ({ onClick, completed, text}) =>(
+    <li 
+        onClick = {onClick}
+        style= {{
+            textDecoration: completed ? 'line-through' : 'none'
+        }}
+        >
+        {text}
+    </li>
+)
+
+Todo.PropTypes = {
+    onClick: PropTypes.func.isRequired,
+    completed: PropTypes.bool.isRequired,
+    text: PropTypes.string.isRequired
+}
+
+export default Todo
