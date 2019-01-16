f4785ba6bb111350f8000e39928f1e5602e7f362
Sankar V <s_venkat_99@yahoo.co.uk>
2019-01-16 19:51:31 +0530

    comments props for CommentList

--- mousetracker/src/fullstackreact/redux/comment/Comment.js
+++ mousetracker/src/fullstackreact/redux/comment/Comment.js
@@ -1,3 +1,10 @@
+let comments = [
+    {id: 1, author: "Cory Brown", text: "My 2 scents"},
+    {id: 2, author: "Jared Anderson", text: "Let me put it this way. You've heard of Socrates? Aristotle? Plato? Morons!"},
+    {id: 3, author: "Matt Poulson", text: "It's just a function!"},
+    {id: 4, author: "Bruce Campbell", text: "Fish in a tree? How can that be?"}
+];
+
 const Comment = (props) => (
     <div className="comment">
         <h1 className ="commentAuthor">
@@ -6,4 +13,4 @@ const Comment = (props) => (
         {props.children}
     </div>
 );
-
+export default Comment;
--- mousetracker/src/fullstackreact/redux/comment/CommentList.js
+++ mousetracker/src/fullstackreact/redux/comment/CommentList.js
@@ -1,13 +1,11 @@
-let comments = [
-    {id: 1, author: "Cory Brown", text: "My 2 scents"},
-    {id: 2, author: "Jared Anderson", text: "Let me put it this way. You've heard of Socrates? Aristotle? Plato? Morons!"},
-    {id: 3, author: "Matt Poulson", text: "It's just a function!"},
-    {id: 4, author: "Bruce Campbell", text: "Fish in a tree? How can that be?"}
-];
-
-const CommentList = () => (
-    comments.map((comment) =>{
-        <Comment key={comment.id} author= {comment.author} />
+const CommentList = (props) => (
+    <div>
+    {
+        props.comments.map(comment =>(
+        <Comment key={comment.id} author= {comment.author} >
+            {comment.text}
+        </Comment>
+        ))
     }
-)
+    </div>
 );
