8fc3fa65159408e0fe3e04ec041bda4f702ea274
Sankar V <s_venkat_99@yahoo.co.uk>
2019-01-17 17:55:35 +0530

    Version 2 - wiring up store.getState() to CommentList and CommentForm

--- mousetracker/src/fullstackreact/redux/comment/index.js
+++ mousetracker/src/fullstackreact/redux/comment/index.js
@@ -5,6 +5,8 @@ import { App } from './containers/App';
 import commentsReducer  from './reducers';
 import { data } from './components/Comment';
 import { addComment } from './actions';
+import CommentList from './components/CommentList';
+import CommentForm from './components/CommentForm';
 
 const store = createStore (commentsReducer);
 
@@ -13,6 +15,16 @@ const { getState, dispatch } = store;
 data.map(comment => dispatch(addComment(comment)));
 console.log(getState());
 
+/*
 export const render = () => ReactDOM.render(
     <App/>, document.getElementById("demo1")
+)
+*/
+
+export const render = () => ReactDOM.render(
+    <div className = "commentBox">
+        <CommentList comments={store.getState().items} />
+        <CommentForm author={store.getState().author}
+                text={store.getState().text} />
+    </div>, document.getElementById("demo1")
 )
