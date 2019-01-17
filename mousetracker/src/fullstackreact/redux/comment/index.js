74144f9b13ab2fa62d6cb6a5f4acdad82df7e636
Sankar V <s_venkat_99@yahoo.co.uk>
2019-01-17 13:31:42 +0530

    Testing redux through bare dispatch

--- mousetracker/src/fullstackreact/redux/comment/index.js
+++ mousetracker/src/fullstackreact/redux/comment/index.js
@@ -3,8 +3,15 @@ import ReactDOM from 'react-dom';
 import { createStore } from 'redux';
 import { App } from './containers/App';
 import commentsReducer  from './reducers';
+import { data } from './components/Comment';
+import { addComment } from './actions';
 
 const store = createStore (commentsReducer);
+
+//testing
+const { getState, dispatch } = store;
+data.map(comment => dispatch(addComment(comment)));
+console.log(getState());
 
 export const render = () => ReactDOM.render(
     <App/>, document.getElementById("demo1")
