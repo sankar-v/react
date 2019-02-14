54e52a1b37a82754ce31e95cc30069fe3f8eb9fa
Sankar V <s_venkat_99@yahoo.co.uk>
2019-02-14 18:39:35 +0530

    Semantic UI download and incorporation, Product and Product List classes

--- mousetracker/src/fullstackreact/voting_app/ProductList.js
+++ mousetracker/src/fullstackreact/voting_app/ProductList.js
@@ -1,13 +1,62 @@
 import React from 'react';
 import Product from './Product';
+import Seed from './seed';
 
 class ProductList extends React.Component {
+
+    constructor(props){
+        super(props);
+        this.state = {
+            products: [],
+        };
+    }
+
+    componentDidMount(){
+        this.setState({products: new Seed().products});
+    }
+
+    handleProductUpVote(productID) {
+        console.log(productID + 'was upvoted.');
+        alert(productID);
+        const nextProducts = this.state.products.map((product) => {
+            if (product.id === productID){
+                return Object.assign({}, product, {
+                    votes: product.votes + 1
+                })
+            }else{
+                return product;
+            }
+        });
+
+        this.setState({
+            products: nextProducts
+        });
+    }
+
     render() {
-      return (
-        <div className='ui unstackable items'>
-          <Product />
-        </div>
-      );
+        const products = this.state.products.sort((a,b) => (
+            b.votes - a.votes
+        ));
+
+        const productComponents = products.map((product) => (
+            <Product
+                key={'product-' + product.id}
+                id={product.id}
+                title={product.title}
+                description={product.description}
+                url={product.url}
+                votes={product.votes}
+                submitterAvatarUrl={product.submitterAvatarUrl}
+                productImageUrl={product.productImageUrl}
+                onVote={this.handleProductUpVote.bind(this)}
+            />
+        ));
+
+        return (
+            <div className='ui unstackable items'>
+                {productComponents}
+            </div>
+        );
     }
 }
 
