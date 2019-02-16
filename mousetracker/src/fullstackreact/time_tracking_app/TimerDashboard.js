1c03cac9e6b4777288c5d9ab1ae7d935f4341178
Sankar V <s_venkat_99@yahoo.co.uk>
2019-02-16 12:54:11 +0530

    Passing properties as state from Parent TimerDashboard to EditableTimerList

--- mousetracker/src/fullstackreact/time_tracking_app/TimerDashboard.js
+++ mousetracker/src/fullstackreact/time_tracking_app/TimerDashboard.js
@@ -3,11 +3,31 @@ import EditableTimerList from './EditableTimerList';
 import ToggleableTimerForm from './ToggleableTimerForm';
 
 class TimerDashboard extends React.Component{
+    state = {
+        timers:[
+            {
+                title: 'Practice squat',
+                project: 'Gym chores',
+                id: '12345',
+                elapsed: 546099,
+                runningSince: Date.now(),
+            },
+            {
+                title: 'Bake Squash',
+                project: 'Kitchen Chores',
+                id: '345678',
+                elapsed: 1273998,
+                runningSince: null,
+            },
+        ],
+    };
+
     render(){
         return(
             <div className='ui three column centered grid'>
                 <div className='column'>
-                    <EditableTimerList />
+                    <EditableTimerList 
+                        timers = {this.state.timers}/>
                     <ToggleableTimerForm
                         isOpen={false}
                     />
