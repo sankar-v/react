eaa524b7c7cee1cbfb5c5aeee7e3a6b6a5975054
Sankar V <s_venkat_99@yahoo.co.uk>
2019-02-17 12:40:45 +0530

    Adding state to ToggleableTimerForm, TimerForm, EditableTimer components

--- mousetracker/src/fullstackreact/time_tracking_app/TimerForm.js
+++ mousetracker/src/fullstackreact/time_tracking_app/TimerForm.js
@@ -1,6 +1,21 @@
 import React from 'react';
 
 class TimerForm extends React.Component{
+    state = {
+        title: this.props.title || '',
+        project: this.props.project || '',
+    };
+
+    handleTitleChange = (e) =>{
+        let title = e.target.value;
+        this.setState({title:title});
+    }
+
+    handleProjectChange = (e) => {
+        let project = e.target.value;
+        this.setState({project: project}); 
+    }
+
     render(){
         const submitText = this.props.title ? 'Update' : 'Create';
         return(
@@ -9,11 +24,17 @@ class TimerForm extends React.Component{
                     <div className='ui form'>
                         <div className='field'>
                             <label>Title</label>
-                            <input type='text' defaultValue={this.props.title} />
+                            <input 
+                                type='text' 
+                                value={this.state.title} 
+                                onChange={this.handleTitleChange}/>
                         </div>
                         <div className='field'>
                             <label>Project</label>
-                            <input type='text' defaultValue={this.props.project} />
+                            <input 
+                                type='text' 
+                                value={this.state.project}
+                                onChange={this.handleProjectChange} />
                         </div>
                         <div className='ui two bottom attached buttons'>
                             <button className = 'ui basic blue button'>
