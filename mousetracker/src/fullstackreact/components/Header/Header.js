43fbe6df030c5d7a1452cf086fc97f1066857c62
Sankar V <s_venkat_99@yahoo.co.uk>
2019-03-17 20:06:23 +0530

    Header class  form with defaultProps

--- mousetracker/src/fullstackreact/components/Header/Header.js
+++ mousetracker/src/fullstackreact/components/Header/Header.js
@@ -1,11 +1,27 @@
 import React from 'react';
 
+/*
 const Header = (props) =>(
     <h1>{props.headerText} { props.label} </h1>
 );
+*/
 
+class Header extends React.Component{
+    static defaultProps = {
+        headerText: "Hello world",
+        label: " How are you"
+    }; 
+
+    render(){
+        return(
+        <h1>{this.props.headerText} {this.props.label}</h1>
+        );
+    }
+}
+/*
 Header.defaultProps = {
     headerText : 'Hello world',
-    label: "How are you"
+    label: "I am fine"
 }
+*/
 export default Header;
